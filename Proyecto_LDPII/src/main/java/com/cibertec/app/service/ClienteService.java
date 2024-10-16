e0e752956a8537c8cd293c3496dfcaa3de0fa8ca
Gimblet <gimbletdev@gmail.com>
2024-10-16 00:36:27 -0500

    agregar clases de guia

---
+++ Proyecto_LDPII/src/main/java/com/cibertec/app/service/ClienteService.java
@@ -0,0 +1,9 @@
+package com.cibertec.app.service;
+
+import java.util.List;
+
+import com.cibertec.app.entity.Cliente;
+
+public interface ClienteService {
+	public List<Cliente> getAllCliente();
+}
