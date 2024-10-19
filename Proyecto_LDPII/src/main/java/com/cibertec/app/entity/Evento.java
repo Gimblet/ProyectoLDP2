ea27dc1e401a6b700f2d94a6d02cdc0fc5b65d4b
Gimblet <gimbletdev@gmail.com>
2024-10-18 23:06:17 -0500

    Cambio a integer para el repository

--- Proyecto_LDPII/src/main/java/com/cibertec/app/entity/Evento.java
+++ Proyecto_LDPII/src/main/java/com/cibertec/app/entity/Evento.java
@@ -18,7 +18,7 @@ public class Evento implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int idEvento;
+	private Integer idEvento;
 
 	@Basic(optional = false)
 	@Column(name = "nombreEvento")
