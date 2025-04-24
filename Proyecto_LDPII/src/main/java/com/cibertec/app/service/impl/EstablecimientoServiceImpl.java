10727cc08b1815f1c9dea49f74d145bd5fb51b34
Dante-Diaz-20 <zevaztian.20@gmail.com>
2025-04-24 11:03:42 -0500

    Implementacion de ServiceImpl y Controller de Establecimiento

--- Proyecto_LDPII/src/main/java/com/cibertec/app/service/impl/EstablecimientoServiceImpl.java
+++ Proyecto_LDPII/src/main/java/com/cibertec/app/service/impl/EstablecimientoServiceImpl.java
@@ -3,39 +3,56 @@ package com.cibertec.app.service.impl;
 import com.cibertec.app.dto.establecimiento.EstablecimientoRequestDTO;
 import com.cibertec.app.dto.establecimiento.EstablecimientoResponseDTO;
 import com.cibertec.app.entity.Establecimiento;
+import com.cibertec.app.mapper.establecimiento.EstablecimientoMapper;
 import com.cibertec.app.repository.EstablecimientoRepository;
 import com.cibertec.app.service.EstablecimientoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class EstablecimientoServiceImpl implements EstablecimientoService {
+    private final EstablecimientoRepository establecimientoRepository;
+    private final EstablecimientoMapper establecimientoMapper;
 
 
     @Override
     public List<EstablecimientoResponseDTO> listar() {
-        return List.of();
+        return establecimientoRepository.findAll().stream()
+                .map(establecimientoMapper::toDto)
+                .toList();
     }
 
     @Override
     public EstablecimientoResponseDTO guardar(EstablecimientoRequestDTO requestDTO) {
-        return null;
+        Establecimiento establecimiento = establecimientoMapper.toEntity(requestDTO);
+        Establecimiento establecimientoGuardado = establecimientoRepository.save(establecimiento);
+        return establecimientoMapper.toDto(establecimientoGuardado);
     }
 
     @Override
     public EstablecimientoResponseDTO obtenerPorId(Long id) {
-        return null;
+        return establecimientoRepository.findById(id)
+                .map(establecimientoMapper::toDto)
+                .orElseThrow(() -> new RuntimeException("Establecimiento no encontrado"));
     }
 
     @Override
     public void eliminar(Long id) {
-
+        if (!establecimientoRepository.existsById(id)){
+            throw new RuntimeException("No existe establecimiento con ID : " + id);
+        }
+        establecimientoRepository.deleteById(id);
     }
+
+    //
 
     @Override
     public Establecimiento obtenerEntidadPorId(Long id) {
-        return null;
+        return establecimientoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Establecimiento no encontrado"));
     }
 }
 
