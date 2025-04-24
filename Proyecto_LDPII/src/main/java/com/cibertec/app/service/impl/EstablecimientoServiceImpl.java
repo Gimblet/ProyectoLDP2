cbba246818256c9c9517a6003315f840675b29ac
Dante-Diaz-20 <zevaztian.20@gmail.com>
2025-04-23 22:47:00 -0500

    implementacion de los DTO,Service y Mapper de la entidad establecimiento

--- Proyecto_LDPII/src/main/java/com/cibertec/app/service/impl/EstablecimientoServiceImpl.java
+++ Proyecto_LDPII/src/main/java/com/cibertec/app/service/impl/EstablecimientoServiceImpl.java
@@ -1,5 +1,7 @@
 package com.cibertec.app.service.impl;
 
+import com.cibertec.app.dto.establecimiento.EstablecimientoRequestDTO;
+import com.cibertec.app.dto.establecimiento.EstablecimientoResponseDTO;
 import com.cibertec.app.entity.Establecimiento;
 import com.cibertec.app.repository.EstablecimientoRepository;
 import com.cibertec.app.service.EstablecimientoService;
@@ -10,14 +12,32 @@ import java.util.List;
 @Service
 public class EstablecimientoServiceImpl implements EstablecimientoService {
 
-    EstablecimientoRepository establecimientoRepository;
 
-    public EstablecimientoServiceImpl(EstablecimientoRepository establecimientoRepository) {
-        this.establecimientoRepository = establecimientoRepository;
+    @Override
+    public List<EstablecimientoResponseDTO> listar() {
+        return List.of();
     }
 
     @Override
-    public List<Establecimiento> getAllEstablecimiento() {
-        return establecimientoRepository.findAll();
+    public EstablecimientoResponseDTO guardar(EstablecimientoRequestDTO requestDTO) {
+        return null;
+    }
+
+    @Override
+    public EstablecimientoResponseDTO obtenerPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void eliminar(Long id) {
+
+    }
+
+    @Override
+    public Establecimiento obtenerEntidadPorId(Long id) {
+        return null;
     }
 }
+
+
+
